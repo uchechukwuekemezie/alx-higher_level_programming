03123dd9edf23636fa1daeb8ea5311e2ac779fcb
uchechukwuekemezie <ucheemekaekemezie@gmail.com>
2023-03-08 15:18:28 -0800

    Print comb

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
-        if i == 99:
-                print(i)
-        else:
-                print(f"{}".format('0' + str(i) if i < 10 else i), end=", ")
+for number in range(0, 100):
+            if number == 99:
+                            print("{}".format(number))
+            else:
+                            print("{:02}".format(number), end=", ")
