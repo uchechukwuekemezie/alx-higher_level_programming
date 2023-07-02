32bca1dc65d4f8d39f118c798c923123388ba14b
uchechukwuekemezie <ucheemekaekemezie@gmail.com>
2023-07-03 00:16:42 +0100

    Write a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-#a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-
-url="$1"
-response=$(curl -s -w "%{http_code}" "$url")
-
-status_code=$(echo "$response" | tail -n1)
-body=$(echo "$response" | sed '$d')
-
-if [ "$status_code" = "200" ]; then
-    echo "$body"
-else
-    echo "Error: Status code $status_code"
-fi
+# This script Sends a GET request to the URL and store the response body
+curl -sf "$1"
