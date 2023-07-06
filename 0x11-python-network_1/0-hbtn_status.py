3398df2defbccd9112e7feb4a2dd73cc6add7fe9
uchechukwuekemezie <ucheemekaekemezie@gmail.com>
2023-07-06 03:16:43 +0100

    Write a Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+script that fetches https://alx-intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    my_url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(my_url) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", content.decode('utf-8'))
